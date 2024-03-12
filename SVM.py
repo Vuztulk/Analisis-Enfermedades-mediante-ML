3c50055961a3700e93aa22f98bf61575c5bd134d
Vuztulk <luisbalo2001@gmail.com>
2024-03-12 13:14:25 +0100

    a

--- SVM.py
+++ SVM.py
@@ -13,16 +13,14 @@ y_train = df_train['Disease']  # Etiquetas
 X_test = df_test.drop('Disease', axis=1)
 y_test = df_test['Disease']
 
-# Codificación de las etiquetas
+# Codificacion etiquetas
 le = LabelEncoder()
-y_train = le.fit_transform(y_train)
 
-# Eliminamos las filas con etiquetas desconocidas en el conjunto de datos de prueba
-df_test = df_test[df_test['Disease'].isin(df_train['Disease'].unique())]
+# Quitar etiquetas de test que no estan en train para que no haya errores
+all_labels = pd.concat([df_train['Disease'], df_test['Disease']])
+le.fit(all_labels)
 
-X_test = df_test.drop('Disease', axis=1)
-y_test = df_test['Disease']
-
+y_train = le.transform(y_train)
 y_test = le.transform(y_test)
 
 model = svm.SVC()
