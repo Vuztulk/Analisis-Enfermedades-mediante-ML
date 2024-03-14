5da3df4cc6934034ba32cc0e4ec6c8f8789af347
Vuztulk <luisbalo2001@gmail.com>
2024-03-14 14:33:53 +0100

    a

--- LogisticRegresion.py
+++ LogisticRegresion.py
@@ -1,11 +1,8 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, auc, classification_report
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv("./blood_samples.csv")
